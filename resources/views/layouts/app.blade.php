9904578d36ae449d187d463d4089d0d34ad95012
Galih Satria <galihsatria@gmail.com>
2016-11-17 10:09:02 +0700

    Jakarta smart CCTV Bali Tower

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
             <div class="collapse navbar-collapse" id="spark-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/about') }}">About</a></li>
+                    <li><a href="{{ url('/about') }}/">About</a></li>
                 </ul>
             </div>
         </div>
